2b660657b4c710af3cceb45ca77d230fccb4010a
MatiC26 <117606695+MatiC26@users.noreply.github.com>
2026-05-21 14:03:31 -0300

    Carga de jutsus

---
+++ Jutsus añadidos.sql
@@ -0,0 +1,133 @@
+INSERT INTO jutsus (nombre, tipo) 
+VALUES 
+-- Naruto
+('Rasengan', 'Ninjutsu'),
+('Rasen Shuriken', 'Ninjutsu'),
+('Kage Bunshin no Jutsu', 'Ninjutsu'),
+('Sage Mode', 'Senjutsu'),
+('Bijuu Dama', 'Ninjutsu'),
+-- Sasuke
+('Chidori', 'Ninjutsu'),
+('Amaterasu', 'Dojutsu'),
+('Susanoo', 'Dojutsu'),
+('Kirin', 'Ninjutsu'),
+('Amenotejikara', 'Dojutsu'),
+-- Sakura Haruno
+('Byakugo no Jutsu', 'Fūinjutsu'),
+('Shosen Jutsu', 'Ninjutsu médico'),
+('Katsuyu Kuchiyose', 'Kuchiyose/Ninjutsu'),
+('Cherry Blossom Impact', 'Taijutsu'),
+-- Kakashi Hatake
+('Raikiri', 'Ninjutsu'),
+('Kamui', 'Dojutsu'),
+('Chidori', 'Ninjutsu'),
+('Doton: Doryuheki', 'Ninjutsu'),
+('Purple Lightning', 'Ninjutsu'),
+-- Hashirama Senju
+('Shinsu Senju', 'Ninjutsu'),
+('Mokuryu no Jutsu', 'Ninjutsu'),
+('Jukai Kotan', 'Ninjutsu'),
+('Kajukai Korin', 'Ninjutsu'),
+('Sage Mode', 'Senjutsu'),
+-- Tobirama Senju
+('Hiraishin no Jutsu', 'Ninjutsu espacial'),
+('Edo Tensei', 'Kinjutsu'),
+('Suiton: Suidanha', 'Ninjutsu'),
+('Kage Bunshin no Jutsu', 'Ninjutsu'),
+('Gojokibaku Fuda', 'Ninjutsu'),
+-- Asuma Sarutobi
+('Katon: Haisekisho', 'Ninjutsu'),
+('Futon: Dust Cloud Technique', 'Ninjutsu'),
+('Thousand-Armed Murder', 'Ninjutsu'),
+('Wind Release Enhancement', 'Ninjutsu'),
+-- Itachi Uchiha
+('Tsukuyomi', 'Genjutsu/Dojutsu'),
+('Amaterasu', 'Dojutsu'),
+('Susanoo', 'Dojutsu'),
+('Izanami', 'Genjutsu'),
+('Yasaka Magatama', 'Ninjutsu'),
+-- Minato Namikaze
+('Hiraishin no Jutsu', 'Ninjutsu espacial'),
+('Rasengan', 'Ninjutsu'),
+('Shiki Fujin', 'Fūinjutsu'),
+('Flying Thunder God Slash', 'Kenjutsu'),
+-- Hiruzen Sarutobi
+('Shiki Fujin', 'Fūinjutsu'),
+('Enma Kuchiyose', 'Kuchiyose/Ninjutsu'),
+('Elemental Ninjutsu Mastery', 'Ninjutsu'),
+('Adamantine Staff', 'Bukijutsu'),
+-- Ino Yamanaka
+('Shintenshin no Jutsu', 'Hiden/Ninjutsu'),
+('Mind Body Disturbance Technique', 'Hiden/Ninjutsu'),
+('Mind Transmission', 'Hiden/Ninjutsu'),
+-- Shikamaru Nara
+('Kagemane no Jutsu', 'Hiden/Ninjutsu'),
+('Kage Nui', 'Hiden/Ninjutsu'),
+('Shadow Strangle Technique', 'Hiden/Ninjutsu'),
+('Kageyose', 'Hiden/Ninjutsu'),
+('Shadow Sewing', 'Hiden/Ninjutsu'),
+-- Choji Akimichi
+('Baika no Jutsu', 'Hiden/Ninjutsu'),
+('Nikudan Sensha', 'Taijutsu'),
+('Butterfly Mode', 'Hiden'),
+('Cho Harite', 'Taijutsu'),
+('Bubun Baika no Jutsu', 'Hiden/Ninjutsu'),
+-- Neji Hyuga
+('Byakugan', 'Dojutsu'),
+('Hakke Rokujuyon Sho', 'Taijutsu'),
+('Kaiten', 'Taijutsu'),
+('Juken', 'Taijutsu'),
+('Hakke Kuhekisho', 'Taijutsu'),
+-- Hinata Hyuga
+('Byakugan', 'Dojutsu'),
+('Twin Lion Fists', 'Taijutsu'),
+('Hakke Kusho', 'Taijutsu'),
+('Juken', 'Taijutsu'),
+('Protective Eight Trigrams', 'Taijutsu'),
+-- Kiba Inuzuka
+('Gatsuuga', 'Taijutsu'),
+('Garoga', 'Taijutsu'),
+('Beast Clone', 'Ninjutsu'),
+('Fang Over Fang', 'Taijutsu'),
+('Three-Headed Wolf Transformation', 'Ninjutsu'),
+-- Shino Aburame
+('Kikaichu no Jutsu', 'Hiden/Ninjutsu'),
+('Insect Sphere', 'Hiden/Ninjutsu'),
+('Parasite Giant Bugs', 'Hiden/Ninjutsu'),
+('Insect Clone Technique', 'Hiden/Ninjutsu'),
+('Beetle Wall', 'Hiden/Ninjutsu'),
+-- Jiraiya 
+('Sage Mode', 'Senjutsu'),
+('Rasengan', 'Ninjutsu'),
+('Frog Song Genjutsu', 'Genjutsu'),
+('Yomi Numa', 'Ninjutsu'),
+('Kuchiyose: Gamabunta', 'Kuchiyose/Ninjutsu'),
+-- Tsunade 
+('Byakugo no Jutsu', 'Fūinjutsu'),
+('Regeneración Mitótica', 'Ninjutsu médico'),
+('Super fuerza', 'Taijutsu'),
+('Katsuyu Kuchiyose', 'Kuchiyose/Ninjutsu'),
+-- Gaara
+('Sabaku Kyuu', 'Ninjutsu'),
+('Sabaku Sousou', 'Ninjutsu'),
+('Shield of Sand', 'Ninjutsu'),
+('Sand Tsunami', 'Ninjutsu'),
+('Desert Imperial Funeral', 'Ninjutsu'),
+-- Madara uchiha
+('Susanoo Perfecto', 'Dojutsu'),
+('Limbo', 'Dojutsu'),
+('Tengai Shinsei', 'Ninjutsu'),
+('Infinite Tsukuyomi', 'Genjutsu'),
+('Chibaku Tensei', 'Ninjutsu'),
+-- Obito Uchiha
+('Kamui', 'Dojutsu'),
+('Izanagi', 'Genjutsu'),
+('Gudodama', 'Ninjutsu'),
+('Mokuton', 'Ninjutsu'),
+('Six Paths Techniques', 'Ninjutsu'),
+-- Orochimaru
+('Edo Tensei', 'Kinjutsu'),
+('Kusanagi no Tsurugi', 'Kenjutsu'),
+('Fushi Tensei', 'Kinjutsu'),
+('Kuchiyose: Manda', 'Kuchiyose/Ninjutsu'),
+('Juinjutsu (Curse Mark)', 'Fūinjutsu');
